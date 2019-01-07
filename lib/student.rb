c87a8f1294fa4250c4f97a154019131016acc908
Grace Choi <oiograceoio@gmail.com>
2019-01-07 04:25:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,12 @@
 class Student < User
-
-  def initialize=(knowledge)
+  attr_accessor :knowledge
+  
+  def initialize
     @knowledge = []
   end
 
   def learn(knowledge)
-    @knowledge < knowledge
+    @knowledge << knowledge
   end
 
   def knowledge
